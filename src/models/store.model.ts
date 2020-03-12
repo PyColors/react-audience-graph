bc65ec8ccd2ec95ecd5469cfc4afc96b01b24506
PyColors <patweb44@gmail.com>
2020-03-12 23:58:19 +0100

    Update models: add ipsService and authService

--- src/models/store.model.ts
+++ src/models/store.model.ts
@@ -1,9 +1,15 @@
 import { LocaleModel, localeModel } from "./locale.model";
+import { AuthModel, authModel } from "./auth.model";
+import { IpsModel, ipsModel } from "./ips.model";
 
 export interface StoreModel {
   locale: LocaleModel;
+  auth: AuthModel;
+  ips: IpsModel;
 }
 
 export const storeModel: StoreModel = {
-  locale: localeModel
+  locale: localeModel,
+  auth: authModel,
+  ips: ipsModel
 };
