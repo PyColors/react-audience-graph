0fa3800330e01a7ce1ec44507b391b1944a14f16
PyColors <patweb44@gmail.com>
2020-03-13 00:01:03 +0100

    Make requests with Axios

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,10 +1,12 @@
-import React, { Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import { useFetch } from "../../hooks/api.hook";
+import { useStoreActions, useStoreState } from "../../hooks/store.hook";
 
 import { RouterProps } from "../../interfaces/router-props.interface";
 import { APP_NAME } from "../../constants/constants";
@@ -19,19 +21,12 @@ import {
 } from "recharts";
 
 const data = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
+  { name: "Page A", uv: 4000, pv: 2400, coco: 2400 },
+  { name: "Page B", uv: 3000, pv: 1398, coco: 2210 },
+  { name: "Page C", uv: 2000, pv: 9800, coco: 2290 },
+  { name: "Page D", uv: 2780, pv: 3908, coco: 2000 },
+  { name: "Page E", uv: 1890, pv: 4800, coco: 2181 },
+  { name: "Page F", uv: 2390, pv: 3800, coco: 2500 }
 ];
 
 const useStyles = makeStyles(theme => ({
@@ -57,7 +52,20 @@ const useStyles = makeStyles(theme => ({
 interface HomeProps extends RouterProps {}
 
 const Home = ({ history: { push } }: HomeProps) => {
+  const isLoading = useStoreState(state => state.ips.isLoading);
+  const ips = useStoreState(state => state.ips.items);
+  const fetch = useStoreActions(actions => actions.ips.discuss);
+  const reset = useStoreActions(actions => actions.ips.reset);
+  const fetchCallback = useFetch(fetch);
+
   const classes = useStyles();
+
+  useEffect(() => {
+    fetchCallback();
+    return () => {
+      reset();
+    };
+  }, [fetchCallback, reset]);
 
   return (
     <Fragment key="home">
@@ -73,7 +81,15 @@ const Home = ({ history: { push } }: HomeProps) => {
                 </Typography>
               </Paper>
             </Grid>
-
+            {isLoading ? (
+              <span>Loading...</span>
+            ) : (
+              <ul>
+                {ips.map((title, index) => (
+                  <li key={index}>{title.percentage}</li>
+                ))}
+              </ul>
+            )}
             <Grid item xs={12} sm={6}>
               <Paper className={classes.paper}>
                 <Typography variant="h6" component="h2" gutterBottom>
@@ -114,7 +130,6 @@ const Home = ({ history: { push } }: HomeProps) => {
                 </AreaChart>
               </Paper>
             </Grid>
-
             <Grid item xs={12} sm={6}>
               <Paper className={classes.paper}>
                 <Typography variant="h6" component="h2" gutterBottom>
@@ -147,7 +162,7 @@ const Home = ({ history: { push } }: HomeProps) => {
                   />
                   <Area
                     type="monotone"
-                    dataKey="amt"
+                    dataKey="coco"
                     stackId="1"
                     stroke="#ffc658"
                     fill="#ffc658"
