2973f285aed993ffe7e97823df3088f7d74874da
PyColors <patweb44@gmail.com>
2020-03-12 23:59:43 +0100

    Update interfaces

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  clientid: number;
+  company: string;
+  fname: string;
+  lname: string;
+  email: string;
+  website: string;
+  timestamp: number;
+  description: string;
+}
